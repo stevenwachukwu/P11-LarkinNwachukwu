2fbd21d3d5c943c87dac216fb3989dfd39038189
Steve Nwachukwu <144286017+stevenwachukwu@users.noreply.github.com>
2024-04-27 01:55:15 -0400

    Create Enum.cpp

---
+++ Enum.cpp
@@ -0,0 +1,5 @@
+/*Written by Steve Nwachukwu and Nick Larkin*/
+#include "Enum.hpp"
+const string colorStrings[] = { "White", "Orange", "Yellow", "Green", "Blue", "Red"};
+
+const char MarkerColorStrings[] = {'W', 'O', 'Y', 'G', 'B'};
