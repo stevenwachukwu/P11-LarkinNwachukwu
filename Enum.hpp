1c499b35f75443ca3934b64ff132de64dbde7eb1
Steve Nwachukwu <144286017+stevenwachukwu@users.noreply.github.com>
2024-04-27 01:55:40 -0400

    Create Enum.hpp

---
+++ Enum.hpp
@@ -0,0 +1,19 @@
+/*Written by Steve Nwachukwu and Nick Larkin*/
+#ifndef ENUMS_H
+#define ENUMS_H
+#include "tools.hpp"
+
+
+enum class ECcolor {
+    white,orange,yellow,green,blue,red
+    /*,red Red is the Error color */
+};
+
+
+enum class MarkerColor {
+    white, orange, yellow, green, blue
+};
+extern const string colorStrings[];
+extern const char MarkerColorStrings[];
+
+#endif
